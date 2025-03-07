f9906f9d1793d5ce4a26f6fa66c738b0a4ff8176
aprogrammer <andrey@aprogrammer.ru>
2025-03-07 22:47:16 +0300

    Add tests with snapshots

--- src/components/User/Users.spec.tsx
+++ src/components/User/Users.spec.tsx
@@ -14,4 +14,34 @@ describe("User", () => {
     const users = await screen.findAllByRole("listitem");
     expect(users).toHaveLength(2);
   });
+
+  // Тест проверяет по снимку, что компонент Users отрисовывается корректно
+  test('test an inline snapshot', () => {
+    const {asFragment, debug} = render(<Users />) // Возвращает объект с методом asFragment, который возвращает текущий снимок компонента
+    debug(); // Выводит в консоль DOM компонента
+    expect(asFragment()).toMatchInlineSnapshot(`
+<DocumentFragment>
+  <div>
+    <h1>
+      Users
+    </h1>
+    <ul>
+      <li>
+        name 1
+      </li>
+      <li>
+        name 2
+      </li>
+    </ul>
+  </div>
+</DocumentFragment>
+`) // если не передать здесь строку, то будет сгенерирована строка из предыдущего вызова
+  })
+
+  // Тест сохраняет снимок в файл и проверяет, что он соответствует фактическому снимку
+  it('using a snapshot file', () => {
+    const {asFragment} = render(<Users />)
+    expect(asFragment()).toMatchSnapshot();
+  });
+  
 });
